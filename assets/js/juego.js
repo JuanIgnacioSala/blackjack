f6cb717ac042dd7e9bf15f9efe5e85f038aa2f0b
JuanIgnacioSala <JuanIgnacioSala5@hotmail.com>
2021-01-12 17:22:51 -0300

    Finish

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -10,13 +10,15 @@ const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['J', 'Q', 'K', 'A']
 
 let puntosJugador = 0;
-let puntosComputador = 0;
+let puntosComputadora = 0;
 const puntosHTML = document.querySelectorAll('small');
 
 //referencias
 const cartasJugador = document.querySelector('#jugador-cartas')
-const cartasComputadoras = document.querySelector('#computadora-cartas')
+const cartasComputadora = document.querySelector('#computadora-cartas')
 const pedir = document.querySelector('#pedir');
+const detener = document.querySelector('#detener');
+const nuevo = document.querySelector('#nuevo');
 
 //creamos un nuevo Deck
 const crearDeck = () => {
@@ -60,7 +62,6 @@ const valorCarta = (card) => {
         return (isNaN(valor)) ?
             (valor === 'A') ? 11 : 10 :
             valor * 1;
-
     }
     // let puntos = 0;
     // if (isNaN(valor)) {
@@ -68,10 +69,44 @@ const valorCarta = (card) => {
     // } else {
     //     console.log('es un numero');
     //     puntos = valor * 1;
+    // }
+    // console.log(puntos);
+    // -------------------------------------------------
 
-// }
-// console.log(puntos);
+//Turno de la computadora
 
+const turnoComputadora = (puntosMin) => {
+
+    do {
+
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        puntosHTML[1].innerText = puntosComputadora;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        cartasComputadora.append(imgCarta);
+
+        if (puntosMin > 21) {
+            break;
+        }
+
+    } while (puntosComputadora < puntosMin && (puntosMin <= 21));
+
+    setTimeout(() => {
+        if (puntosMin > puntosComputadora && puntosMin <= 21) {
+            alert('Ganaste Genio !!');
+        } else if (puntosMin === puntosComputadora) {
+            alert('Nadie Gana');
+        } else if (puntosComputadora > 21) {
+            alert('Ganaste Genio !!');
+        } else {
+            alert('Compu Wins');
+        }
+    }, 10);
+}
 
 //eventos
 pedir.addEventListener('click', () => {
@@ -89,10 +124,39 @@ pedir.addEventListener('click', () => {
     if (puntosJugador > 21) {
         console.warn('Perdiste');
         pedir.disabled = true;
+        detener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
-        console.warn('21, Joya');
+        console.warn('21, puntaje perfecto.');
         pedir.disabled = true;
+        detener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
+});
+
+detener.addEventListener('click', () => {
+    pedir.disabled = true;
+    detener.disabled = true;
+    turnoComputadora(puntosJugador);
+
+});
 
 
-})
+
+nuevo.addEventListener('click', () => {
+    console.clear();
+    deck = [];
+    crearDeck();
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+
+    cartasJugador.innerHTML = '';
+    cartasComputadora.innerHTML = '';
+
+    pedir.disabled = false;
+    detener.disabled = false;
+
+});
